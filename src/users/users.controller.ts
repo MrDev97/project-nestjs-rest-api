fe76e248dd0d8d6a30d5bc650d131feeb5d34627
Mikolaj Renke <mikolaj.renke@gmail.com>
2022-09-27 22:28:57 +0200

    Update UserModule Controller

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,8 +1,19 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Param,
+  Body,
+  Post,
+  HttpCode,
+  Delete,
+  Put,
+} from '@nestjs/common';
 import { dateToArray } from 'src/shared/date.helper';
 import { User } from './interfaces/user.interface';
 import { UsersDataService } from './users-data-service';
 import { ExternalUserDto } from './dto/external-user.dto';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
 
 @Controller('users')
 export class UsersController {
@@ -11,10 +22,32 @@ export class UsersController {
     return this.mapUserToExternal(this.userRepository.getUserById(_id_));
   }
 
+  @Get()
+  getAllUsers(): Array<User> {
+    return this.userRepository.getAllUsers();
+  }
+
+  @Post()
+  addUser(@Body() item: CreateUserDto): ExternalUserDto {
+    return this.mapUserToExternal(this.userRepository.addUser(item));
+  }
+
   mapUserToExternal(user: User): ExternalUserDto {
     return {
       ...user,
       birthday: dateToArray(user.birthday),
     };
+  }
+
+  @Delete(':id') @HttpCode(204) deleteUser(@Param('id') _id_: string): void {
+    this.userRepository.deleteUser(_id_);
+  }
+
+  @Put(':id')
+  updateUser(
+    @Param('id') _id_: string,
+    @Body() item: UpdateUserDto,
+  ): ExternalUserDto {
+    return this.mapUserToExternal(this.userRepository.updateUser(_id_, item));
   }
 }
