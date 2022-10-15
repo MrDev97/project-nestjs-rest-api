49a55e0d091d43e14d8d1320ca163ab3d92c3fe6
Mikolaj Renke <mikolaj.renke@gmail.com>
2022-10-15 11:25:08 +0200

    Add update-product-dto validation

--- src/products/dto/update-product.dto.ts
+++ src/products/dto/update-product.dto.ts
@@ -1,8 +1,27 @@
 import { Tags } from '../enums/tags.enum';
+import {
+  MinLength,
+  MaxLength,
+  Min,
+  IsNumber,
+  IsEnum,
+  IsArray,
+} from 'class-validator';
 
-export interface UpdateProductDto {
+export class UpdateProductDto {
+  @MinLength(1)
+  @MaxLength(25)
   name: string;
+
+  @Min(0)
+  @IsNumber()
   price: number;
+
+  @Min(0)
+  @IsNumber()
   count: number;
+
+  @IsEnum({ each: true })
+  @IsArray()
   tags: Array<Tags>;
 }
