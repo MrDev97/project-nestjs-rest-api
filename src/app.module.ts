85a854f41c0175141883d9f25569af01ab0349fa
Mikolaj Renke <mikolaj.renke@gmail.com>
2022-10-16 12:12:07 +0200

    Implement LoggerInterceptor for whole app

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,12 +9,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductsModule } from './products/products.module';
 import { UsersModule } from './users/users.module';
-import { LoggerService } from './shared/interceptors/logger.service';
 
 @Module({
   imports: [ProductsModule, UsersModule],
   controllers: [AppController],
-  providers: [AppService, LoggerService],
+  providers: [AppService],
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer): void {
