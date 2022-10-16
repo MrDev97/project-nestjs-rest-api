6f61e6662231f16c1e413709fa9fd292d101cb23
Mikolaj Renke <mikolaj.renke@gmail.com>
2022-10-16 11:19:53 +0200

    Fix update-user.dto

--- src/users/dto/update-user.dto.ts
+++ src/users/dto/update-user.dto.ts
@@ -1,15 +1,18 @@
 import { Roles } from '../enums/roles.enum';
+import { Transform } from 'class-transformer';
+import { arrayToDate } from '../../shared/date.helper';
 
-export interface UpdateUserDto {
+export class UpdateUserDto {
   name: string;
   surname: string;
   email: string;
+  @Transform((d) => arrayToDate(d))
   dateOfBirth: Date;
   adress?: Array<UserAddress>;
   role: Roles;
 }
 
-export interface UserAddress {
+export class UserAddress {
   country: string;
   city: string;
   street: string;
